58452f6bc6865024f1b9cbf85cea2a9bfe0624d9
Jamieson Leibovitch <31860909+jleibovitch@users.noreply.github.com>
2017-10-02 20:31:13 -0400

    Command line arguments actually work now

--- projectile.cpp
+++ projectile.cpp
@@ -65,9 +65,9 @@ void n_steps(unsigned n, TState state0, Type type)  {
 
 int main(int argc, char* argv[]) {
 
-  if(argv[1] == "v" || argv[1] == "V")
+  if (argv[1] == std::string("-v") || argv[1] == std::string("-V"))
     n_steps(12000, TState{0., {-E_0 / (w * w * sqrt(2)), 0, 0}, {0, E_0 / (w * sqrt(2)), -.25}}, Type::VERLET);
-  if(argv[1] == "e" || argv[1] == "E")
+  else if (argv[1] == std::string("-e") || argv[1] == std::string("-E"))
     n_steps(12000, TState{0., {-E_0 / (w * w * sqrt(2)), 0, 0}, {0, E_0 / (w * sqrt(2)), -.25}}, Type::EULER);
   return 0;
 }
